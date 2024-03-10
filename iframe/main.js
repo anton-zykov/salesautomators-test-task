dd54414b5bea213db5f6af9b91f19afe4eea9595
Zykov Anton <anton.zykov1@gmail.com>
2024-03-10 12:51:49 +0100

    Add form markup, styles, ability to save fields to local storage

--- iframe/main.js
+++ iframe/main.js
@@ -1,5 +1,40 @@
 import 'https://cdn.jsdelivr.net/npm/@pipedrive/app-extensions-sdk@0/dist/index.umd.js';
 
 (async function() {
-  const sdk = await new AppExtensionsSDK().initialize();
+  //const sdk = await new AppExtensionsSDK().initialize();
+  const form = document.getElementById('jobForm');
+
+  for (const name of Object.keys(localStorage)) {
+    form.querySelector(`[name="${name}"]`).value = localStorage.getItem(name);
+  }
+
+  form.querySelector('input[name="startTime"]').addEventListener('change', (event) => {
+    form.querySelector('input[name="endTime"]').min = event.target.value;
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    
+    localStorage.clear();
+  };
+
+  const handleSave = (event) => {
+    const formData = new FormData(form);
+    for (const [name, value] of formData.entries()) {
+      localStorage.setItem(name, value);
+    }
+
+    const button = event.target;
+    button.textContent = 'Saved!';
+    button.classList.add('form__button_success');
+    setTimeout(() => {
+      button.textContent = 'Save info';
+      button.classList.remove('form__button_success');
+    }, 3000);
+  };
+
+  form.addEventListener('submit', handleSubmit);
+  form.querySelector('.form__button_save').addEventListener('click', handleSave);
 })();
+
