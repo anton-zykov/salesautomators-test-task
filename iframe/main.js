eacdd43992d99dbf53ac9d65af81d9c121308ef2
Zykov Anton <anton.zykov1@gmail.com>
2024-03-10 13:11:11 +0100

    Adjust sizes of iframe

--- iframe/main.js
+++ iframe/main.js
@@ -1,7 +1,12 @@
 import 'https://cdn.jsdelivr.net/npm/@pipedrive/app-extensions-sdk@0/dist/index.umd.js';
 
 (async function() {
-  const sdk = await new AppExtensionsSDK().initialize();
+  const sdk = await new AppExtensionsSDK().initialize({
+    size: {
+      height: 700,
+      width: 700,
+    }, 
+  });
   const form = document.getElementById('jobForm');
 
   for (const name of Object.keys(localStorage)) {
